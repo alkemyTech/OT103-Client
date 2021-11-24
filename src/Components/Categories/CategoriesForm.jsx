ae7c87754824db6fbe4bfcca81503fbf05bc8a61
Julian Kominovic <juliankominovic@gmail.com>
2021-11-24 14:04:08 -0300

    component refactor

--- src/Components/Categories/CategoriesForm.jsx
+++ src/Components/Categories/CategoriesForm.jsx
@@ -1,16 +1,15 @@
-import React, { useRef, useState, useEffect } from "react";
+import React from "react";
 import { Formik, Form } from "formik";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import classicEditor from "@ckeditor/ckeditor5-build-classic";
 import { validateCategoryForm } from "../../schemas/categoryFormValidation";
 import "./CategoryForm.scss";
 import {
   modifyCategory,
   uploadCategory,
 } from "../../Services/privateApi/categoryApi";
-import getBase64FromUrl from "../../helpers/imageToBase64";
-
-//TODO refactor
+import useCategoriesForm from "../../hooks/useCategoriesForm";
+import InputImageFile from "./InputImageFile";
+import InputText from "./InputText";
+import InputCkEditor from "./InputCkEditor";
 
 /*
 
@@ -35,37 +34,45 @@ OUTPUTS => Form component and api call
 */
 
 const CategoriesForm = ({ category }) => {
-  //FOR IMAGE PREVIEW
-  //AND SENDING TO API
-  //See FILE API from html5
-  const fileReader = new FileReader();
-  fileReader.onload = (e) => {
-    setImagePreview(e.target.result);
+  const { imageInputRef, imagePreview, status, setStatus, fileReader } =
+    useCategoriesForm(category);
+
+  const handleSubmit = (values) => {
+    category === undefined
+      ? uploadCategory({ ...values, image: imagePreview })
+          .then((res) => {
+            setStatus(res);
+          })
+          .catch((err) => {
+            setStatus(err);
+          })
+      : modifyCategory({
+          ...values,
+          image: imagePreview,
+          id: category.id,
+        })
+          .then((res) => {
+            setStatus(res);
+          })
+          .catch((err) => {
+            setStatus(err);
+          });
   };
 
-  const imageInputRef = useRef();
-  //   FOR SENDING THE IMAGE IN BASE64 and also previewing it on the frontend
-  const [imagePreview, setImagePreview] = useState();
+  const emptyCategoryData = {
+    name: "",
+    description: "",
+    image: "",
+  };
 
-  const [status, setStatus] = useState("");
-
-  useEffect(() => {
-    if (category !== undefined)
-      getBase64FromUrl(category.image)
-        .then((res) => setImagePreview(res))
-        .catch((err) => console.log(err));
-  }, [category]);
-
-  //HIDE THE MESSAGE IN 4 SECONDS
-  // CLEANUP TO PREVENT ERROR DISMOUNTING COMPONENT
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setStatus("");
-    }, 4000);
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [status]);
+  const chooseInitialData =
+    category !== undefined
+      ? {
+          name: category.name,
+          description: category.description,
+          image: category.image,
+        }
+      : emptyCategoryData;
 
   return (
     <>
@@ -73,50 +80,14 @@ const CategoriesForm = ({ category }) => {
         {category ? "Edit category" : "Create category"}
       </h1>
       <Formik
-        initialValues={
-          category !== undefined
-            ? {
-                name: category.name,
-                description: category.description,
-                image: category.image,
-              }
-            : {
-                name: "",
-                description: "",
-                image: "",
-              }
-        }
-        initialErrors={{
-          name: "",
-          description: "",
-          image: "",
-        }}
+        initialValues={chooseInitialData}
+        initialErrors={emptyCategoryData}
         validate={(values) => {
           console.log(values);
           // ALSO VALIDATES IMAGE INPUT
           return validateCategoryForm(values, imageInputRef);
         }}
-        onSubmit={(values) => {
-          category === undefined
-            ? uploadCategory({ ...values, image: imagePreview })
-                .then((res) => {
-                  setStatus(res);
-                })
-                .catch((err) => {
-                  setStatus(err);
-                })
-            : modifyCategory({
-                ...values,
-                image: imagePreview,
-                id: category.id,
-              })
-                .then((res) => {
-                  setStatus(res);
-                })
-                .catch((err) => {
-                  setStatus(err);
-                });
-        }}
+        onSubmit={handleSubmit}
       >
         {({
           handleBlur,
@@ -127,83 +98,38 @@ const CategoriesForm = ({ category }) => {
           setTouched,
         }) => (
           <Form className="form">
-            <div>
-              <label className="form__label" htmlFor="name">
-                Name: <small className="form__label-required">*</small>
-              </label>
-              <input
-                type="text"
-                placeholder="name..."
-                name="name"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className="form__input"
-                defaultValue={values.name}
-              />
-              {errors.name && touched.name && (
-                <div className="form__input-error">{errors.name}</div>
-              )}
-            </div>
+            <InputText
+              name="name"
+              required={true}
+              handleChange={handleChange}
+              handleBlur={handleBlur}
+              defaultValue={values.name}
+              values={values}
+              errors={errors}
+              touched={touched}
+            />
+            <InputImageFile
+              name="image"
+              values={values}
+              handleBlur={handleBlur}
+              fileReader={fileReader}
+              handleChange={handleChange}
+              imageInputRef={imageInputRef}
+              errors={errors}
+              touched={touched}
+              imagePreview={imagePreview}
+              required={true}
+            />
 
-            <div>
-              <label
-                className="form__label"
-                htmlFor="image"
-                onBlur={handleBlur}
-              >
-                {values.image || (
-                  <span>
-                    Choose an image...
-                    <small className="form__label-required">*</small>
-                  </span>
-                )}
-              </label>
-              <input
-                type="file"
-                name="image"
-                id="image"
-                ref={imageInputRef}
-                onChange={(e) => {
-                  handleChange(e);
-                  fileReader.readAsDataURL(e.target.files[0]);
-                }}
-                className="form__input"
-                accept="image/png,  image/jpeg"
-              />
-              {errors.image && touched.image && (
-                <div className="form__input-error">{errors.image}</div>
-              )}
-              {values.image ? (
-                <div>
-                  <img
-                    src={imagePreview}
-                    className="form__image-preview"
-                    alt="preview"
-                  />
-                </div>
-              ) : null}
-            </div>
-
-            <div>
-              <label className="form__label" htmlFor="description">
-                Description: <small className="form__label-required">*</small>
-              </label>
-              <CKEditor
-                className="form__input"
-                name="description"
-                editor={classicEditor}
-                data={category ? category.description : ""}
-                onChange={(e, editor) =>
-                  (values.description = editor.getData())
-                }
-                onBlur={() => {
-                  setTouched({ ...touched, description: true });
-                }}
-              />
-              {errors.description && touched.description && (
-                <div className="form__input-error">{errors.description}</div>
-              )}
-            </div>
+            <InputCkEditor
+              name="description"
+              required={true}
+              values={values}
+              defaultObjectValues={category}
+              setTouched={setTouched}
+              touched={touched}
+              errors={errors}
+            />
 
             <button className="form__button" type="submit">
               Submit
---
+++ src/Components/Categories/InputText.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const InputText = ({
+  name,
+  required,
+  handleChange,
+  handleBlur,
+  defaultValue,
+  values,
+  errors,
+  touched,
+}) => {
+  return (
+    <div>
+      <label className="form__label" htmlFor={name}>
+        {name}: {required && <small className="form__label-required">*</small>}
+      </label>
+      <input
+        type="text"
+        placeholder={`${name}...`}
+        name={name}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        className="form__input"
+        defaultValue={values[name]}
+      />
+      {errors[name] && touched[name] && (
+        <div className="form__input-error">{errors[name]}</div>
+      )}
+    </div>
+  );
+};
+
+export default InputText;
