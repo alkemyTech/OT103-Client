338ddd8fe57c9480f9fd75018fd712ef9c9aef5e
Julian Kominovic <juliankominovic@gmail.com>
2021-11-24 21:14:04 -0300

    Console.log and tests removed

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-
-      <App />
-
->>>>>>> d0717def0941e28b82f9f40b5302b54aaca14f21
   </React.StrictMode>,
   document.getElementById("root")
 );
