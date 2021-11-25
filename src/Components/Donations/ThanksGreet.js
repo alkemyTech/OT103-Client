0144fc290a05a6bd0aaf86f680281addc384b785
Lucas <lucasmoauro.93@gmail.com>
2021-11-24 22:23:37 -0300

    Fix: Removed inline styles

--- src/Components/Donations/ThanksGreet.js
+++ src/Components/Donations/ThanksGreet.js
@@ -1,6 +1,9 @@
+import './styles/thankGreet.scss'
+
+
 export const ThanksGreet = ({ title = "¡Muchas gracias por tu donacion!" }) => {
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    <div className="thanks-main-div" >
       <h1> {title} </h1>
     </div>
   );
