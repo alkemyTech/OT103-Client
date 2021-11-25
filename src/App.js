99b25394c64d9991b55ad87047d4f090cb0c133f
nahueldefazio <nahuel1116@gmail.com>
2021-11-25 00:50:46 -0300

    Added '/edit-user/:id' path and edit UserForm component

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
           <Route path="/backoffice/create-slide" component={SlidesForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
+          <Route path="/edit-user/:id" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
--- src/Components/Users/UsersForm.js
+++ src/Components/Users/UsersForm.js
@@ -1,38 +1,126 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import {Field, Form, Formik} from "formik";
+import * as Yup from "yup";
 import '../FormStyles.css';
+import axios from "axios";
+import {useParams} from "react-router-dom";
 
 const UserForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        email: '',
-        roleId: ''
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [role_id, setRole_id] = useState("");
+    const [create, setCreate] = useState(true);
+
+    const {id} = useParams()
+    const url = `http://ongapi.alkemy.org/api/users`
+
+    const submitForm = (values) => {
+        if (create) {
+            axios.post(`${url}`, {
+                name: values.title,
+                email: values.email,
+                role_id: values.role_id,
+                profile_image: values.profile_image.name,
+            })
+                .then(function (response) {
+                    setName(response.data.name)
+                    setEmail(response.data.email)
+                    setRole_id(response.data.role_id)
+                })
+
+        } else {
+            axios.put(`${url}/${id} `, {
+                name: values.title,
+                email: values.email,
+                role_id: values.role_id,
+                profile_image: values.profile_image.name,
+
+            })
+                .then(function (response) {
+                    setName(response.data.name)
+                    setEmail(response.data.email)
+                    setRole_id(response.data.role_id)
+                })
+        }
+
+
+    }
+
+    useEffect(() => {
+        axios.get(`${url} `)
+            .then(response => {
+                setCreate(false)
+                setName(response.data.data[id].name)
+                setEmail(response.data.data[id].email)
+                setRole_id(response.data.data[id].role_id)
+            })
+            .catch(error => {
+                alert(error)
+            })
+    }, []);
+
+    const ErrorSchema = Yup.object().shape({
+        name: Yup.string().required("Name is required.").min(4, "Name is too short"),
+        email: Yup.string().required("Email is required.").email('Invalid email'),
+        pass: Yup.string().required("Password is required.").min(8, "Password is too short"),
+        role_id: Yup.string().required("Role is required."),
+        profile_image: Yup.string().required("Photo is required.")
     })
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'email'){
-            setInitialValues({...initialValues, email: e.target.value})
-        }
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-    }
-
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name || ''} onChange={handleChange} placeholder="Name"></input>
-            <input className="input-field" type="text" name="email" value={initialValues.description || ''} onChange={handleChange} placeholder="Email"></input>
-            <select className="input-field" value={initialValues.roleId || ''} onChange={e => setInitialValues({...initialValues, roleId: e.target.value})}>
-                <option value="" disabled >Select the role</option>
-                <option value="1">Admin</option>
-                <option value="2">User</option>
-            </select>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
+        <div>
+            <Formik initialValues={{name, email, role_id}} onSubmit={(values => {
+                submitForm(values)
+            })} validationSchema={ErrorSchema}
+                    enableReinitialize={true}
+            >
+                {
+                    (props) => {
+                        return (
+                            <Form>
+                                <div>
+                                    <h3>Information</h3>
+                                    <label>Name: </label>
+                                    <Field name={'name'} type={'text'}/>
+                                    <small>{props.errors.name}</small>
+                                    <label>Email: </label>
+                                    <Field name={'email'} type={'email'}/>
+                                    <small>{props.errors.email}</small>
+                                    <label>Password: </label>
+                                    <Field name={'pass'} type={'password'}/>
+                                    <small>{props.errors.pass}</small>
+                                    <label>Role: </label>
+                                    <Field name={'role_id'} as="select">
+                                        <option value="">-- Select role --</option>
+                                        <option value="0">User</option>
+                                        <option value="1">Administrator</option>
+                                    </Field>
+                                    <small>{props.errors.role_id}</small>
+                                    <input
+                                        type="file"
+                                        name="profile_image"
+                                        accept="image/png,image/jpeg"
+                                        onChange={(event) => {
+                                            props.setFieldValue("profile_image", event.target.files[0]);
+                                        }}
+                                    />
+                                    <button type={'submit'}
+                                            disabled={!props.isValid}> Send
+                                    </button>
+                                </div>
+                            </Form>
+                        )
+                    }
+                }
+            </Formik>
+            <div>
+                {name && <h3><strong>Name:</strong> {name} </h3>}
+                {email && <h3><strong>Email:</strong> {email}</h3>}
+                {role_id && <h3><strong>Role:</strong> {role_id}</h3>}
+            </div>
+        </div>
     );
 }
- 
+
 export default UserForm;
