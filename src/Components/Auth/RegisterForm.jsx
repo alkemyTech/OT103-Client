b50704dae5498205ba88cbda2b14ffa370ce249e
Guido Fortunato <guido_fortunato@hotmail.com.ar>
2021-11-25 12:41:55 -0300

    formulario de registro finalizado

--- src/Components/Auth/RegisterForm.jsx
+++ src/Components/Auth/RegisterForm.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../FormStyles.css';
 import { Formik } from 'formik'
 
 const RegisterForm = () => {
-    // const [initialValues, setInitialValues] = useState({
-    //     name: '',
-    //     lastName: '',
-    //     email: '',
-    //     password: '',
-    //     confirmPassword:''
-    // })
+    
+    const [formEnviado, setFormEnviado] = useState(false)
+    const [initialValues, setInitialValues] = useState({
+        name: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword:''
+    })
     
     // const handleChange = (e) => {
     //     if(e.target.name === 'name'){
@@ -42,32 +44,72 @@ const RegisterForm = () => {
 
 
                 if (!values.name) {
-                    errores.name('Please enter a name')
-                }else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.name)) {
-                    errores.name = 'El nombre solo puede contener letras y espacios'
+                    errores.name = 'Please enter a name'
                 }
+                
+                // else if (!/^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(values.name)) {
+                //     errores.name = 'El nombre solo puede contener letras y espacios'
+                // }
 
                 if (!values.lastName) {
-                    errores.lastName('Please enter a lastName')
+                    errores.lastName = 'Please enter a last name'
                 }
                 if (!values.email) {
-                    errores.email('Please enter an email')
+                    errores.email = 'Please enter an email'
                 }
+
                 if (!values.password) {
-                    errores.password('Please enter a password')
+                    errores.password = 'Please enter a password'
+
+                } else if (values.password.length < 6) {
+                    errores.password = 'Your password must be at least 6 characters'
+
+                } else if (values.password.search(/[a-z]/i) < 0) {
+                    errores.password = "Your password must contain at least one letter."
+
+                } else if (values.password.search(/[0-9]/) < 0) {
+                    errores.password = "Your password must contain at least one digit."
                 }
+
+                else if (values.password.search(/(?=.*[!@#$%^&*])/)) {
+                    errores.password = "Your password must contain at least one special character."
+                }
+
+
+
+
+
+
                 if (!values.confirmPassword) {
-                    errores.confirmPassword('Please confirm password')
+                    errores.confirmPassword = 'Please confirm password'
+                } else if (values.confirmPassword !== values.password){
+                    errores.confirmPassword = 'Passwords can not be different'
                 }
 
                 return errores
             }}
 
             onSubmit={(values, {resetForm}) => {
-                resetForm()
-                console.log('form enviado')
-                console.log(values)
+                setFormEnviado(true)
                 localStorage.setItem('token', 'tokenValueExample')
+                resetForm()
+                setTimeout(() => {
+                    setFormEnviado(false)
+                }, 3000);
+                setInitialValues({
+                    name: values.name,
+                    lastName: values.lastName,
+                    email: values.email,
+                    password: values.password,
+                })
+                alert(
+                    `
+                    Name: ${initialValues.name}
+                    Last name: ${initialValues.lastName}
+                    Email: ${initialValues.email}
+                    Password: ${initialValues.password}                    
+                    `
+                )
             }}
         >
 
@@ -76,75 +118,83 @@ const RegisterForm = () => {
                     <input 
                         className="input-field" 
                         type="text" 
-                        name="name" 
+                        name="name"
+                        id='name'
                         value={values.name} 
                         onChange={handleChange} 
                         placeholder="Enter name"
                         onBlur={handleBlur}
-                    >
+                    />
 
-                        {touched.name && errors.name && <div className='error'>{ errors.name }</div>}
+                    {touched.name && errors.name && <div className='error'>{ errors.name }</div>}
                         
-                    </input>
+                        
+                    
 
                     <input 
                         className="input-field" 
                         type="text" 
-                        name="lastName" 
+                        name="lastName"
+                        id='lastName'
                         value={values.lastName}
                         onChange={handleChange} 
                         placeholder="Enter last name"
                         onBlur={handleBlur}
-                    >
+                    />
                         {touched.lastName && errors.lastName && <div className='error'>{errors.lastName}</div>}
                         
-                        </input>
+                        
 
                     <input 
                         className="input-field" 
                         type="email" 
-                        name="email" 
+                        name="email"
+                        id='email'
                         value={values.email} 
                         onChange={handleChange} 
                         placeholder="Enter email"
                         onBlur={handleBlur}
-                    >
+                    />
                         {touched.email && errors.email && <div className='error'>{errors.email}</div>}
 
-                    </input>
+                    
 
                     <input 
                         className="input-field" 
                         type="password" 
-                        name="password" 
+                        name="password"
+                        id='password'
                         value={values.password} 
                         onChange={handleChange} 
                         placeholder="Enter password"
                         onBlur={handleBlur}
-                    >
+                    />
                         {touched.password && errors.password && <div className='error'>{errors.password}</div>}
                         
-                        </input>
+                        
 
                     <input 
                         className="input-field" 
                         type="password" 
-                        name="confirmPassword" 
+                        name="confirmPassword"
+                        id='confirmPassword'
                         value={values.confirmPassword} 
                         onChange={handleChange} 
                         placeholder="Confirm password"
                         onBlur={handleBlur}
-                    >
+                    />
                         {touched.confirmPassword && errors.confirmPassword && <div className='error'>{errors.confirmPassword}</div>}
                         
-                        </input>
+                        
                     <button
                         className="submit-btn"
                         type="submit">
                         Register
                     </button>
 
-                    <p className="form-success">Form enviado con éxito</p>
+                    {
+                       formEnviado && <p className="form-success">Form enviado con éxito</p>
+                    }
                 </form>
                 
             )}
