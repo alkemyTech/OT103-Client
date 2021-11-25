52ac22de11d4b27451bf2aac867aff3f5f0bdde9
Guido Fortunato <guido_fortunato@hotmail.com.ar>
2021-11-25 19:30:47 -0300

    login finalizado

--- src/Components/Auth/LoginForm.jsx
+++ src/Components/Auth/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../FormStyles.css';
+import { Formik } from 'formik'
 
 const LoginForm = () => {
     const [initialValues, setInitialValues] = useState({
@@ -7,26 +8,110 @@ const LoginForm = () => {
         password: ''
     });
 
-    const handleChange = (e) => {
-        if(e.target.name === 'email'){
-            setInitialValues({...initialValues, email: e.target.value})
-        } if(e.target.name === 'password'){
-            setInitialValues({...initialValues, password: e.target.value})
-        }
-    }
-    
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
-        localStorage.setItem('token', 'tokenValueExample')
-    }
+    const [formEnviado, setFormEnviado] = useState(false)
+
 
     return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="email" value={initialValues.name} onChange={handleChange} placeholder="Enter email"></input>
-            <input className="input-field" type="text" name="password" value={initialValues.password} onChange={handleChange} placeholder="Enter password"></input>
-            <button className="submit-btn" type="submit">Log In</button>
-        </form>
+        <Formik
+            initialValues = {{                
+                email: '',
+                password: '',                          
+            }}
+
+            validate={(values) => {
+
+                let errores = {}
+                
+                if (!values.email.trim()) {
+                    errores.email = 'Please enter an email'
+                }
+                
+                if (!values.password.trim()) {
+                    errores.password = 'Please enter a password'
+
+                } else if (values.password.length < 6) {
+                    errores.password = 'Your password must be at least 6 characters'
+
+                } else if (values.password.search(/[a-z]/i) < 0) {
+                    errores.password = "Your password must contain at least one letter."
+
+                } else if (values.password.search(/[0-9]/) < 0) {
+                    errores.password = "Your password must contain at least one digit."
+                }
+
+                else if (values.password.search(/(?=.*[!@#$%^&*])/)) {
+                    errores.password = "Your password must contain at least one special character."
+                }
+
+                return errores
+                
+
+
+
+
+            }}
+            onSubmit={(values, {resetForm}) => {
+                
+                setInitialValues({
+                    email: values.email,
+                    password: values.password,
+                })
+                //localStorage.setItem('token', 'tokenValueExample')
+                setFormEnviado(true)
+                setTimeout(() => {
+                    setFormEnviado(false)
+                }, 10000);
+                alert(
+                    `
+                    Email: ${values.email}
+                    Password: ${values.password}                    
+                    `
+                )
+                resetForm()
+            }}
+        
+        >
+
+            {({errors, values,touched, handleSubmit, handleChange, handleBlur}) => (
+                <form className="form-container" onSubmit={handleSubmit}>
+                    <input 
+                        className="input-field"
+                        id='email'
+                        type="email" 
+                        name="email" 
+                        value={values.email} 
+                        onChange={handleChange}
+                        placeholder="Enter email"
+                        onBlur={handleBlur}
+                    />
+
+                    {
+                        touched.email && errors.email && <div className='form-error'>{errors.email}</div>
+                    }
+
+                    <input 
+                        className="input-field"
+                        id='password'
+                        type="password" 
+                        name="password" 
+                        value={values.password} 
+                        onChange={handleChange} 
+                        placeholder="Enter password"
+                        onBlur={handleBlur}
+                    />
+                    {
+                        touched.password && errors.password && <div className='form-error'>{errors.password}</div>
+                    }
+                        
+                        
+                    <button className="submit-btn" type="submit">Log In</button>
+                    {
+                        formEnviado && <p className="form-success">Log in successfull</p>
+                    }
+                </form>                
+            )}
+
+        </Formik>
     );
 }
  
