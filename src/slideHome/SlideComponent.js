ad6b27ac2437e20cb07de98280f4b3c3ac140824
decdabo <david.caro.dec@hotmail.com>
2021-11-25 09:13:59 -0300

    slide class

--- src/slideHome/SlideComponent.js
+++ src/slideHome/SlideComponent.js
@@ -4,14 +4,9 @@ export const SlideComponent = ({ data }) => {
     return (
         <div style={{
             backgroundImage: `url(${data.image})`,
-            backgroundPosition: 'center',
-            backgroundSize: '100vw',
-            height: '30vh',
-            maxHeight: `250px`,
-            display: 'flex',
-            alignItems: 'end',
-            justifyContent: 'center'
-        }}>
+        }}
+        className="slide__home"
+        >
             <h1>{data.name}</h1>
         </div>
     );
