e00d46dbf79f8855c7d39f0c38f6f837d1f6eb5c
Lucas <lucasmoauro.93@gmail.com>
2021-11-25 19:25:18 -0300

    Validations added

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,32 +1,104 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React from "react";
+
+import { Field, useFormik } from "formik";
+import * as Yup from "yup";
+
+import "../FormStyles.css";
 
 const SlidesForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        description: ''
-    });
+  /*TODO:
+ - Terminar las validaciones {
+    Todos los campos son obligatorios
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
-    }
+    Name debe tener una longitud mínima de 4 caracteres 
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(initialValues);
-    }
+    Órder debe ser único
 
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Slide Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write the description"></input>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
-    );
-}
- 
+    La imagen deberá tener un formato .jpg o .png
+ } (TODO: ver si se puede pasar a helper la funcion validate de formik)
+ - Ver de usar Formik en vez de useFormik
+ - Ver video de yup
+ - Reemplazar descripcion por el componente ckEditor
+ - Lograr reutilizacion del form tanto para editar como crear
+ - Llamada api call POST / PATCH condicional
+ - Creacion de ruta /backoffice/slides
+
+*/
+
+  function formSubmit() {
+    console.log(formik.values);
+  }
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      description: "",
+      order: 0,
+      image: "",
+    },
+    validate: (values) => {
+      const errors = {};
+
+      if (!values.name) {
+        errors.name = "El nombre es obligatorio";
+      } else if (values.name.length < 4) {
+        errors.name = "El nombre debe tener 4 caracteres o mas";
+      }
+      if (!values.description) {
+        errors.description = "La descripcion es obligatoria.";
+      }
+      if (!values.order) {
+        errors.order = "El orden es obligatorio.";
+      }
+      if (!values.image) {
+        errors.image = "La imagen es obligatoria.";
+      }
+      return errors;
+    },
+    onSubmit: () => {
+      if (!formik.isValid) {
+        console.log("form invalid");
+        return;
+      }
+
+      formSubmit();
+    },
+  });
+
+  return (
+    <form className="form-container" onSubmit={formik.handleSubmit}>
+      <Field
+        className="input-field"
+        type="text"
+        {...formik.getFieldProps("name")}
+        placeholder="Slide Title"
+      />
+      {formik.touched.name && formik.errors.name ? (
+        <div>{formik.errors.name}</div>
+      ) : null}
+      <Field
+        className="input-field"
+        type="text"
+        {...formik.getFieldProps("description")}
+        placeholder="Write the description"
+      />
+      <Field
+        className="input-field"
+        type="text"
+        {...formik.getFieldProps("order")}
+        placeholder=""
+      />
+      <Field
+        className="input-field"
+        type="file"
+        {...formik.getFieldProps("image")}
+        placeholder="Write the description"
+      />
+      <button className="submit-btn" type="submit">
+        Send
+      </button>
+    </form>
+  );
+};
+
 export default SlidesForm;
