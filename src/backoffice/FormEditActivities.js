f1fd972553184cdeb4aad10b74f2691e247f8c47
decdabo <david.caro.dec@hotmail.com>
2021-11-26 12:05:09 -0300

    formEdit

--- src/backoffice/FormEditActivities.js
+++ src/backoffice/FormEditActivities.js
@@ -1,40 +1,37 @@
 import React from 'react';
 import { Formik } from 'formik';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import parse from 'html-react-parser';
 import axios from 'axios';
+
+import InputImageFile from '../Components/Categories/InputImageFile';
+import useCategoriesForm from '../hooks/useCategoriesForm';
+import InputCkEditor from '../Components/Categories/InputCkEditor';
+import InputText from '../Components/Categories/InputText';
+import { validateCategoryForm } from '../schemas/categoryFormValidation';
+import './formedit.scss'
 
 const initialValues = {
     name: '',
-    description: ''
+    description: '',
+    image: ''
 }
 
 export const FormEditActivities = ({ activities = initialValues }) => {
-
-    const handleValues = (values) => {
-        let error = {}
-        if (!values.name) {
-            error.name = 'Please complete the space';
-        } else if (values.name.length < 2) {
-            error.name = 'Title too short';
-        }
-        if (!values.description) {
-            error.description = 'Please complete the space';
-        } else if (values.description.length < 2) {
-            error.name = 'Title too short';
-        }
-        return error;
-    }
+    const { imageInputRef, imagePreview, fileReader } =
+        useCategoriesForm(activities);
 
     return (
         <Formik
             initialValues={{
                 name: activities.name,
                 description: activities.description,
+                image: activities.image,
+            }}
+            validate={(values) => {
+                return validateCategoryForm(values, imageInputRef);
             }}
             onSubmit={(values) => {
                 if (!activities.id) {
+                    console.log(values)
                     axios.post('http://ongapi.alkemy.org/api/activities', values)
                         .then(res => alert(res))
                         .catch(e => alert(e))
@@ -45,58 +42,48 @@ export const FormEditActivities = ({ activities = initialValues }) => {
                 }
 
             }}
-            validate={handleValues}
         >
-            {({ values, handleChange, handleBlur, handleSubmit, errors, touched, setFieldValue }) => (
-                <form onSubmit={handleSubmit}>
-                    <div >
-                        <div>
-                            <label>name*</label>
-                            <div>
-                                <input
-                                    autoComplete="off"
-                                    className="form-control bg-dark text-light mb-3"
-                                    id="staticname"
-                                    name="name"
-                                    value={values.name}
-                                    type="text"
-                                    placeholder="Pon un nombre!"
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                />
-                                {touched.name && errors.name && <h5>{errors.name}</h5>}
-                            </div>
-                        </div>
-                        <div>
-                            <CKEditor
-                                editor={ClassicEditor}
-                                data={activities.description}
-                                onChange={(e, editor) => {
-                                    const data = editor.getData();
-                                    if (parse(data).props && parse(data).props.children[1].props) {
-                                        console.log(parse(data).props.children)
-                                        setFieldValue('description', parse(data).props.children);
-                                    } else {
-                                        setFieldValue('description', '');
-                                    }
-                                }}
-                                config={{
-                                    cloudServices: {
-                                        tokenUrl: "https://85122.cke-cs.com/token/dev/63f1e5122f7b89374a44f0ba134c7a670437bab84212188ac1b17d829d92",
-                                        uploadUrl: "https://85122.cke-cs.com/easyimage/upload/",
-                                    },
-                                }}
+            {({ values, handleChange, handleBlur, handleSubmit, errors, touched, setFieldValue, setTouched }) => (
+                <form className="form" onSubmit={handleSubmit}>
+                        <InputText
+                            name="name"
+                            required={true}
+                            handleChange={handleChange}
+                            handleBlur={handleBlur}
+                            defaultValue={values.name}
+                            values={values}
+                            errors={errors}
+                            touched={touched}
+                        />
+                            <InputCkEditor
+                                name="description"
+                                required={true}
+                                values={values}
+                                defaultObjectValues={activities}
+                                setTouched={setTouched}
+                                touched={touched}
+                                errors={errors}
                             />
-                            {touched.description && errors.description && <h5>{errors.description}</h5>}
+                        <div className="form__b-i-conteiner">
+                            <InputImageFile
+                                name="image"
+                                values={values}
+                                handleBlur={handleBlur}
+                                fileReader={fileReader}
+                                handleChange={handleChange}
+                                imageInputRef={imageInputRef}
+                                errors={errors}
+                                touched={touched}
+                                imagePreview={imagePreview}
+                                required={true}
+                            />
+                                <button className="form__button" type="submit">
+                                    Submit
+                                </button>
                         </div>
-                        <div>
-                            <button className="btn col-12 m-auto w-50 text-violet" type="submit">
-                                Login
-                            </button>
-                        </div>
-                    </div>
                 </form>
-            )}
-        </Formik>
+            )
+            }
+        </Formik >
     );
 };
