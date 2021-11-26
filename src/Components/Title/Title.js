64bfe8799d4c12eb7f638bfc6ef2c4a004149aeb
Lucas <lucasmoauro.93@gmail.com>
2021-11-25 22:52:22 -0300

    Removed unnecesary logic

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -3,7 +3,7 @@ import defaultImage from "../../assets/images/404.png";
 export const Title = ({ title, image }) => {
   return (
     <div>
-      <img src={(!image ? defaultImage : image.default) || image} alt="" />
+      <img src={(!image ? defaultImage : image)} alt="" />
       <h1 style={{ textAlign: "center" }}>{title}</h1>
     </div>
   );
