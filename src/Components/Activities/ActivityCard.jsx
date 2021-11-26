8e118fe76bbcf238e5de1c4b4f2f8805a6e43a5a
Julian Kominovic <juliankominovic@gmail.com>
2021-11-25 21:28:03 -0300

    title disabled by the moment

--- src/Components/Activities/ActivityCard.jsx
+++ src/Components/Activities/ActivityCard.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import "./Activities.scss";
-import { Title } from "../Title/Title";
+// import { Title } from "../Title/Title";
 import apiDateToText from "../../helpers/apiDateToText";
 import { Link } from "react-router-dom";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 
 const ActivityCard = ({ activity }) => {
-  const { date, time } = apiDateToText(activity["created_at"]);
+  const { id, description, image, name, created_at } = activity;
+  const { date, time } = apiDateToText(created_at);
   return (
     <li className="list__item">
-      <Title title={activity.name} image={activity.image} />
+      {/* <Title title={activity.name} image={activity.image} /> */}
+      {/* TODO reemplazar h1 e img por el titulo dinamico cuando este terminado*/}
+      <h1>{name}</h1>
+      <img src={image} alt="" />
       <p className="list__item-shutdown-text">
         {date} {time}
       </p>
 
       {/*  esto es mala practica... ver como arreglarlo */}
-      <div dangerouslySetInnerHTML={{ __html: activity.description }}></div>
+      <div dangerouslySetInnerHTML={{ __html: description }}></div>
       <footer>
-        <Link to={`/actividades/${activity.id}`}>
+        <Link to={`/actividades/${id}`}>
           <AiOutlineInfoCircle />
         </Link>
       </footer>
