48c564a5ed29b7a81255daf20d6e3833b805e1cf
Guido Fortunato <guido_fortunato@hotmail.com.ar>
2021-11-28 21:30:03 -0300

    agrego exact a app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,28 +30,28 @@ function App() {
           {/* <Route path="/" exact component={} />           Esta ruta debe ser para el Home */}
           <Route exact path="/actividades" component={ActivitiesList} />
 
-          <Route exactpath="/create-activity" component={ActivitiesForm} />
-          <Route exactpath="/create-category" component={CategoriesForm} />
-          <Route exactpath="/create-news" component={NewsForm} />
-          <Route exactpath="/backoffice/create-slide" component={SlidesForm} />
-          <Route exactpath="/backoffice/organization/edit" component={OrganizationForm} />
-          <Route exactpath="/backoffice/home" component={HomeForm} />
+          <Route exact path="/create-activity" component={ActivitiesForm} />
+          <Route exact path="/create-category" component={CategoriesForm} />
+          <Route exact path="/create-news" component={NewsForm} />
+          <Route exact path="/backoffice/create-slide" component={SlidesForm} />
+          <Route exact path="/backoffice/organization/edit" component={OrganizationForm} />
+          <Route exact path="/backoffice/home" component={HomeForm} />
           <Route exact path="/create-testimonials" component={TestimonialForm} />
-          <Route exactpath="/create-user" component={UserForm} />
-          <Route path="/edit-user/:id" component={UserForm} />
-          <Route exactpath="/create-member" component={MembersForm} />
-          <Route exactpath="/backoffice/members/edit" component={MembersEdit} />
-          <Route exactpath="/create-project" component={ProjectsForm} />
-          <Route exactpath="/school-campaign" component={SchoolCampaign} />
-          <Route exactpath="/toys-campaign" component={ToysCampaign} />
-          <Route exactpath="/backoffice/news/:id" component={NewsForm} />
-          <Route exactpath="/backoffice/news" component={NewsForm} />
-          <Route exactpath="/donar" component={DonationsGreet} />
-          <Route exactpath="/gracias" component={ThanksGreet} />
-          <Route exactpath="/backoffice/slides" component={SlidesForm} />
-          <Route exactpath="/contact-form" component={ContactForm} />
-          <Route exactpath="/register-form" component={RegisterForm} />
-          <Route exactpath="/login-form" component={LoginForm} />
+          <Route exact path="/create-user" component={UserForm} />
+          <Route exact path="/edit-user/:id" component={UserForm} />
+          <Route exact path="/create-member" component={MembersForm} />
+          <Route exact path="/backoffice/members/edit" component={MembersEdit} />
+          <Route exact path="/create-project" component={ProjectsForm} />
+          <Route exact path="/school-campaign" component={SchoolCampaign} />
+          <Route exact path="/toys-campaign" component={ToysCampaign} />
+          <Route exact path="/backoffice/news/:id" component={NewsForm} />
+          <Route exact path="/backoffice/news" component={NewsForm} />
+          <Route exact path="/donar" component={DonationsGreet} />
+          <Route exact path="/gracias" component={ThanksGreet} />
+          <Route exact path="/backoffice/slides" component={SlidesForm} />
+          <Route exact path="/contact-form" component={ContactForm} />
+          <Route exact path="/register-form" component={RegisterForm} />
+          <Route exact path="/login-form" component={LoginForm} />
         </Switch>
       </BrowserRouter>
     </>
