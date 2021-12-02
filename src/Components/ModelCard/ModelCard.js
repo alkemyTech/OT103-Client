fad643731df8e684a2ee82f3bed245319b462ebf
ClaraMariaDS <claradisorbo@gmail.com>
2021-12-02 19:01:49 -0300

    Modelo de card

--- src/Components/ModelCard/ModelCard.js
+++ src/Components/ModelCard/ModelCard.js
@@ -1,19 +1,22 @@
 import React from "react";
+import imgDefault from '../../images/placeholderImg.png';
+import './modelCard.scss'
 
-const CardModel = (props) => {
-    let errorflag=true;
+const CardModel = (props) => {  
+
     return(
-        <div className="images-container">
-            <div>
-                <img src={props.img} alt={props.imgAlt} onError={(e)=>{ if (errorflag){ errorflag=false; e.target.src= "https://www.sedistudio.com.au/wp-content/themes/sedi/assets/images/placeholder/placeholder.png"; } }}
-                />
-                <div className="container">
+            <div className="body-container">   
+                {props.img 
+                ? (<img src={props.img}
+                    alt={props.altImg} className="imgCard"/>)
+                : (<img src={imgDefault}
+                    alt="imgDefault" className="imgCard"/>)
+                }        
+                <div className="info-container">
                     <h4>{props.title}</h4>
                     <p>{props.description}</p>
-                </div>
-             </div>      
-        </div>
-            
+                </div>                  
+            </div>
     );
 }
 export default CardModel;
