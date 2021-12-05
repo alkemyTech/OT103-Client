b2cd5a27d391c988f965d5d6efd8a4bf7eea37fc
nahueldefazio <nahuel1116@gmail.com>
2021-12-05 14:47:21 -0300

    Agregue clases

--- src/Components/HomeCards/HomeCards.jsx
+++ src/Components/HomeCards/HomeCards.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom";
 import "../../utils.scss"
+import "./HomeCards.scss"
 
 const HomeCards = ({title, image}) => {
 
     return (
-        <div className="card m-2 text-center w-18r">
+        <div className="card__home-dashboard my-1r txt-center w-18r">
             <h5 className="card-title">{ title }</h5>
             <div>
                 <img src={ image } className="card-img-top" alt="image"/>
             </div>
             <div className="card-body">
-                <Link to={"/"} className="btn btn-primary">Ir</Link>
+                <Link to={ "/" } className="btn-card" role="button"><span
+                    className="text">Ir</span><span>Ir</span></Link>
             </div>
 
         </div>
