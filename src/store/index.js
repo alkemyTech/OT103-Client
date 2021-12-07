ecfea8de542bc5c056be274278e881bc31744719
Julian Kominovic <juliankominovic@gmail.com>
2021-12-07 12:54:55 -0300

    dev tool enabled

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore } from "redux";
+
+// Put reducers here
+const reducers = combineReducers({});
+
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
