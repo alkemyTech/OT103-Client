d50e31b97e588290f39149da86d819b50c0e1097
Julian Kominovic <juliankominovic@gmail.com>
2021-12-08 15:00:00 -0300

    dotenv package installed

--- src/Components/Activities/ActivityCard.jsx
+++ src/Components/Activities/ActivityCard.jsx
@@ -4,22 +4,23 @@ import apiDateToText from "../../helpers/apiDateToText";
 import { Link } from "react-router-dom";
 
 const ActivityCard = ({ activity }) => {
+  const { name, image, description, id } = activity;
   const { date, time } = apiDateToText(activity["created_at"]);
   return (
     <li className="list__item">
       <Title title={name} image={image} />
-      <div className="list__item-group">
-        <p className="list__item-datetime">
+      <div className="item__group">
+        <p className="item__datetime">
           {date} {time}
         </p>
 
         {/*  esto es mala practica... ver como arreglarlo */}
         <div
-          className="list__item-description"
+          className="item__description"
           dangerouslySetInnerHTML={{ __html: description }}
         ></div>
       </div>
-      <footer className="list__item-footer">
+      <footer className="item__footer">
         <Link to={`/actividades/${id}`} className="list__item-edit-link ">
           Mas info...
         </Link>
