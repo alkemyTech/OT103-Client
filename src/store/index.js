bd67013f0e19f442d150f721b2e000ebd62ee6d4
nahueldefazio <nahuel1116@gmail.com>
2021-12-08 18:38:33 -0300

    Creo el reducer del Slides. Cambie el mock del componente ScreenSliderList.js por el reducer

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,13 @@ import thunk from "redux-thunk";
 // nosotros reducer
 import { membersReducer } from "./reducers/membersReducers";
 import aboutReducer from "./slices/aboutSlice";
+import slidesReducer from "./slices/slidesSlice";
+
 // Put reducers here
 const reducers = { 
   aboutData: aboutReducer,
   members: membersReducer,
+  slidesData: slidesReducer
 };
 
 const store = configureStore({
