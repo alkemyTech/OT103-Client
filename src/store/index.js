f8a19b5a947e6082b2157db3c167bf0bab4c7cfc
Julian Kominovic <juliankominovic@gmail.com>
2021-12-08 10:24:56 -0300

    reducers added to index

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
+import authSlice from "./slices/authSlice";
 // Put reducers here
-const reducers = {};
+const reducers = {
+  authReducer: authSlice,
+};
 
 const store = configureStore({
   reducer: reducers,
