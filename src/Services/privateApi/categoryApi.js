ef6e01718aebe17bc32c4940a1d9b701115ff6b9
ClaraMariaDS <claradisorbo@gmail.com>
2021-12-09 11:18:56 -0300

    OT103-79 fixed

--- src/Services/privateApi/categoryApi.js
+++ src/Services/privateApi/categoryApi.js
@@ -66,7 +66,6 @@ const uploadCategory = (categoryObject) => {
       .then(() => {
         Post('categories', categoryObject)
           .then((res) => {
-            console.log(res);
             if (res.status === 200) {
               resolve(res.data.message);
             } else {
