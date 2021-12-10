f4c3b310f551924de1dcd6e003de1ad685ca754a
David Caro <david.caro.dec@hotmail.com>
2021-12-10 20:22:12 -0300

    changes

--- src/backoffice/SlidesScreen/ScreenSliderList.js
+++ src/backoffice/SlidesScreen/ScreenSliderList.js
@@ -13,7 +13,6 @@ export const ScreenSliderList = () => {
   }, []);
 
   return (
-<<<<<<< HEAD
     <div className="table__main">
       <div className="table__head-container">
         <div className="table__head-title">Listado de Slides</div>
@@ -29,20 +28,3 @@ export const ScreenSliderList = () => {
     </div>
   );
 };
-=======
-    <table className="table__main">
-      <tr className="table__head-container">
-        <th className="table__head-title">Listado de Slides</th>
-        <Link to="/backoffice/Slides/create" className="table__create-button">
-          Create
-        </Link>
-      </tr>
-      <tr>
-        {slidesData.data.map((data, i) => {
-          return <ItemList key={i} data={data} />;
-        })}
-      </tr>
-    </table>
-  );
-};
->>>>>>> af44ea8d2b4585e291d3002deb7305f899e06a48
