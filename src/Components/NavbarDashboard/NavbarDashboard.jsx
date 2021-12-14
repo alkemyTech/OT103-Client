808faae49f4c9c1a73773442e2012aacc5f74b2a
David Caro <david.caro.dec@hotmail.com>
2021-12-14 14:25:37 -0300

    changes and start to sidebar

--- src/Components/NavbarDashboard/NavbarDashboard.jsx
+++ src/Components/NavbarDashboard/NavbarDashboard.jsx
@@ -1,32 +1,30 @@
 import { Link } from "react-router-dom";
-import logo from "../../assets/images/logo.png";
 
 const NavbarDashboard = () => {
-  return (
-    <nav className="backoffice__nav">
-      <img src={logo} className="backoffice__nav-logo" alt="Somos Más" />
-      <ul className="backoffice__nav-link">
-        <strong>
-          <Link className="backoffice__nav-item" to="#">Inicio</Link>
-        </strong>
-        <strong>
-          <Link className="backoffice__nav-item" to="#">Nosotros</Link>
-        </strong>
-        <strong>
-          <Link className="backoffice__nav-item" to="#">Actividades</Link>
-        </strong>
-        <strong>
-          <Link className="backoffice__nav-item" to="#">Novedades</Link>
-        </strong>
-        <strong>
-          <Link className="backoffice__nav-item" to="#">Testimonios</Link>
-        </strong>
-        <strong>
-          <Link className="backoffice__nav-item" to="#">Contacto</Link>
-        </strong>
-      </ul>
-    </nav>
-  );
-};
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"/>
+                </button>
+                <Link className="navbar-brand d-rtl" to={"/"}>
+                    <img src="http://atrilco.com/wp-content/uploads/2017/11/ef3-placeholder-image.jpg" alt="logo"/>
+                </Link>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
 
-export default NavbarDashboard;
+export default NavbarDashboard
