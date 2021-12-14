05d558e80090bfb23d051f7146a4e78102ba8a05
nahueldefazio <nahuel1116@gmail.com>
2021-12-14 18:10:35 -0300

    Creo el componente Footer y lo agrego al App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,80 +31,84 @@ import HomeDashboard from "./Components/HomeDashboard/HomeDashboard";
 import OrganizationData from "./backoffice/Organization/OrganizationData";
 import Members from "./Components/About/Members";
 import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import "./Components/Footer/Footer.scss"
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/actividades" component={ActivitiesList} />
-          <Route exact path="/actividades/:id" component={ActivityInfo} />
-          <Route path="/backoffice" exact component={HomeDashboard} />
-          <Route exact path="/create-activity" component={ActivitiesForm} />
-          <Route exact path="/create-category" component={CategoriesForm} />
-          <Route exact path="/create-news" component={NewsForm} />
-          <Route exact path="/backoffice/Slides" component={ScreenSliderList} />
-          <Route
-            exact
-            path="/backoffice/Slides/create"
-            component={SlidesForm}
-          />
-          <Route
-            exact
-            path="/backoffice/organization/edit"
-            component={OrganizationForm}
-          />
-          <Route exact path="/backoffice/home" component={HomeForm} />
-          <Route
-            exact
-            path="/create-testimonials"
-            component={TestimonialForm}
-          />
-          <Route exact path="/create-user" component={UserForm} />
-          <Route exact path="/edit-user/:id" component={UserForm} />
-          <Route exact path="/create-member" component={MembersForm} />
-          <Route
-            exact
-            path="/backoffice/members/edit/:id"
-            component={MembersEdit}
-          />
-          <Route exact path="/create-project" component={ProjectsForm} />
-          <Route exact path="/school-campaign" component={SchoolCampaign} />
-          <Route exact path="/toys-campaign" component={ToysCampaign} />
-          <Route exact path="/backoffice/news/create" component={NewsForm} />
-          <Route exact path="/backoffice/news/:id" component={NewsForm} />
-          <Route exact path="/backoffice/news" component={NewsList} />
-          <Route
-            exact
-            path="/backoffice/activities"
-            component={ManageActivities}
-          />
-          <Route exact path="/backoffice/users" component={UsersList} />
-          <Route exact path="/donar" component={DonationsGreet} />
-          <Route exact path="/gracias" component={ThanksGreet} />
-          <Route exact path="/contacto" component={Contact} />
-          <Route
-            exact
-            path="/testimonials/create"
-            component={TestimonialForm}
-          />
-          <Route exact path="/testimonials/:id" component={TestimonialForm} />
-          <Route exact path="/about" component={AboutMain} />
-          <Route exact path="/backoffice/slides" component={SlidesForm} />
-          <Route exact path="/contact-form" component={ContactForm} />
-          <Route exact path="/register-form" component={RegisterForm} />
-          <Route exact path="/login-form" component={LoginForm} />
-          <Route
-            exact
-            path="/backoffice/organization"
-            component={OrganizationData}
-          />
-          <Route exact path="/about/members" component={Members} />
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/actividades" component={ ActivitiesList }/>
+                    <Route exact path="/actividades/:id" component={ ActivityInfo }/>
+                    <Route path="/backoffice" exact component={ HomeDashboard }/>
+                    <Route exact path="/create-activity" component={ ActivitiesForm }/>
+                    <Route exact path="/create-category" component={ CategoriesForm }/>
+                    <Route exact path="/create-news" component={ NewsForm }/>
+                    <Route exact path="/backoffice/Slides" component={ ScreenSliderList }/>
+                    <Route
+                        exact
+                        path="/backoffice/Slides/create"
+                        component={ SlidesForm }
+                    />
+                    <Route
+                        exact
+                        path="/backoffice/organization/edit"
+                        component={ OrganizationForm }
+                    />
+                    <Route exact path="/backoffice/home" component={ HomeForm }/>
+                    <Route
+                        exact
+                        path="/create-testimonials"
+                        component={ TestimonialForm }
+                    />
+                    <Route exact path="/create-user" component={ UserForm }/>
+                    <Route exact path="/edit-user/:id" component={ UserForm }/>
+                    <Route exact path="/create-member" component={ MembersForm }/>
+                    <Route
+                        exact
+                        path="/backoffice/members/edit/:id"
+                        component={ MembersEdit }
+                    />
+                    <Route exact path="/create-project" component={ ProjectsForm }/>
+                    <Route exact path="/school-campaign" component={ SchoolCampaign }/>
+                    <Route exact path="/toys-campaign" component={ ToysCampaign }/>
+                    <Route exact path="/backoffice/news/create" component={ NewsForm }/>
+                    <Route exact path="/backoffice/news/:id" component={ NewsForm }/>
+                    <Route exact path="/backoffice/news" component={ NewsList }/>
+                    <Route
+                        exact
+                        path="/backoffice/activities"
+                        component={ ManageActivities }
+                    />
+                    <Route exact path="/backoffice/users" component={ UsersList }/>
+                    <Route exact path="/donar" component={ DonationsGreet }/>
+                    <Route exact path="/gracias" component={ ThanksGreet }/>
+                    <Route exact path="/contacto" component={ Contact }/>
+                    <Route
+                        exact
+                        path="/testimonials/create"
+                        component={ TestimonialForm }
+                    />
+                    <Route exact path="/testimonials/:id" component={ TestimonialForm }/>
+                    <Route exact path="/about" component={ AboutMain }/>
+                    <Route exact path="/backoffice/slides" component={ SlidesForm }/>
+                    <Route exact path="/contact-form" component={ ContactForm }/>
+                    <Route exact path="/register-form" component={ RegisterForm }/>
+                    <Route exact path="/login-form" component={ LoginForm }/>
+                    <Route
+                        exact
+                        path="/backoffice/organization"
+                        component={ OrganizationData }
+                    />
+                    <Route exact path="/about/members" component={ Members }/>
+                    <Route exact path="/" component={ Home }/>
+                </Switch>
+                <Footer/>
+            </BrowserRouter>
+
+        </>
+    );
 }
 
 export default App;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,102 @@
+import { useEffect, useState } from "react";
+import { Get } from "../../Services/privateApiService";
+import linkedinIcon from "../../assets/images/linkedin.ico"
+import twitterIcon from "../../assets/images/twitter.ico"
+import instagramIcon from "../../assets/images/instagram.ico"
+import facebookIcon from "../../assets/images/facebook.ico"
+import { Link } from "react-router-dom";
+
+
+// var style = {
+//     backgroundColor: "#F8F8F8",
+//     borderTop: "1px solid #E7E7E7",
+//     textAlign: "center",
+//     padding: "20px",
+//     position: "relative",
+//     left: "0",
+//     bottom: "0",
+//     height: "60px",
+//     width: "100%",
+// }
+//
+// var phantom = {
+//     display: 'block',
+//     padding: '20px',
+//     height: '60px',
+//     width: '100%',
+// }
+
+const Footer = () => {
+
+    const [name, setName] = useState("");
+    const [logo, setLogo] = useState("");
+    const [facebook_url, setFacebook_url] = useState("");
+    const [linkedin_url, setLinkedin_url] = useState("");
+    const [instagram_url, setInstagram_url] = useState("");
+    const [twitter_url, setTwitter_url] = useState("");
+
+    const getInfoFooter = () => {
+        Get("organization").then(r => {
+            const {data} = r
+            const {
+                name,
+                logo,
+                facebook_url,
+                linkedin_url,
+                instagram_url,
+                twitter_url
+            } = data;
+            setName(name)
+            setLogo(logo)
+            setFacebook_url(facebook_url)
+            setLinkedin_url(linkedin_url)
+            setInstagram_url(instagram_url)
+            setTwitter_url(twitter_url)
+        })
+    }
+
+
+    useEffect(() => {
+        getInfoFooter()
+    }, []);
+
+
+    return (
+        <div className="footer">
+            <div/>
+            <div className="footer__grid">
+                <div className="footer__flex">
+                    <div className="footer__grid__hide">{ name }</div>
+                    <img src={ logo } alt="logo_footer"/>
+                </div>
+                <div className="footer__flex footer__grid__hide" >
+                    <Link to={'/actividades'}>Actividades </Link>
+                    <Link to={'/school-campaign'}> Campaña Colegio</Link>
+                    <Link to={'/toys-campaign'}> Campaña Juguetes</Link>
+                    <Link to={'/contacto'}> Contacto</Link>
+                    <Link to={'/about'}> Nosotros</Link>
+                </div>
+                <div className="footer__flex__icons">
+                    <div>
+                        <a href={ `https://${ facebook_url }` } target="_blank" className="footer__grid__hide">> Facebook</a>
+                        <img src={ facebookIcon } alt="facebookIcon" className="footer__icon"/>
+                    </div>
+                    <div>
+                        <a href={ `https://${ linkedin_url }` } target="_blank" className="footer__grid__hide">>LinkedIn</a>
+                        <img src={ linkedinIcon } alt="linkedinIcon" className="footer__icon"/>
+                    </div>
+                    <div>
+                        <a href={ `https://${ instagram_url }` } target="_blank" className="footer__grid__hide">>Instagram </a>
+                        <img src={ instagramIcon } alt="instagramIcon" className="footer__icon"/>
+                    </div>
+                    <div>
+                        <a href={ `https://${ twitter_url }` } target="_blank" className="footer__grid__hide">>Twitter </a>
+                        <img src={ twitterIcon } alt="twitterIcon" className="footer__icon"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
