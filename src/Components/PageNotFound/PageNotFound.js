373fa8f2b164f2d705ba4423887e9fd21b15631e
Fausto Rossi <faustorossi@usal.edu.ar>
2021-12-16 16:25:10 -0300

    link to home added

--- src/Components/PageNotFound/PageNotFound.js
+++ src/Components/PageNotFound/PageNotFound.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./PageNotFound.scss";
 
 function PageNotFound() {
 	return (
 		<div className="notFound__container">
-			<h1 className="__first-title">404</h1>
-			<i className="__icon far fa-sad-cry"></i>
-			<h2 className="__second-title">Page Not Found</h2>
+			<h1 className="notFound__first-title">404</h1>
+			<i className="notFound__icon far fa-sad-cry"></i>
+			<h2 className="notFound__second-title">Page Not Found</h2>
+			<Link to="/" className="notFound__link">
+				Volver al Inicio
+			</Link>
 		</div>
 	);
 }
