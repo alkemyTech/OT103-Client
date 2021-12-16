8be00b2990ad2805774b6dd0cf26b7859022f930
Fausto Rossi <faustorossi@usal.edu.ar>
2021-12-15 21:15:00 -0300

    conflicts and errors resolved

--- src/Campaigns/Toys/ToysCampaign.js
+++ src/Campaigns/Toys/ToysCampaign.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import Header from "./Header";
-import Slider from "./Slider";
-import Content from "./Content";
-import Footer from "./Footer";
-
-const ToysCampaign = () => {
-	return (
-		<>
-			<Header />
-			<Slider />
-			<Content />
-			<Footer />
-		</>
-	);
-};
- 
-export default ToysCampaign;
-=======
 import "./ToysCampaing.scss";
 import Toy1 from "../../assets/images/toys/1.png";
 import Toy2 from "../../assets/images/toys/2.png";
@@ -35,73 +15,72 @@ import { useEffect, useState } from "react";
 // -------------timer---------------
 
 const CountDownTimer = () => {
-  // timer to 31 december at 17:30
-  const remainingTime = (+new Date("2021-12-31T17:30:59") - +new Date()) / 1000;
+	// timer to 31 december at 17:30
+	const remainingTime = (+new Date("2021-12-31T17:30:59") - +new Date()) / 1000;
 
-  const [time, setTime] = useState(remainingTime);
+	const [time, setTime] = useState(remainingTime);
 
-  useEffect(() => {
-    const timerId = setInterval(() => {
-      setTime((prevState) => prevState - 1);
-    }, 1000);
-    return () => clearInterval(timerId);
-  });
+	useEffect(() => {
+		const timerId = setInterval(() => {
+			setTime((prevState) => prevState - 1);
+		}, 1000);
+		return () => clearInterval(timerId);
+	});
 
-  // format seconds to DD HH:MM:SS
-  const formatTime = (seconds) => {
-    const days = Math.floor(seconds / 86400);
-    seconds = seconds % 86400;
-    const hours = Math.floor(seconds / 3600);
-    seconds = seconds % 3600;
-    const minutes = Math.floor(seconds / 60);
-    seconds = seconds % 60;
+	// format seconds to DD HH:MM:SS
+	const formatTime = (seconds) => {
+		const days = Math.floor(seconds / 86400);
+		seconds = seconds % 86400;
+		const hours = Math.floor(seconds / 3600);
+		seconds = seconds % 3600;
+		const minutes = Math.floor(seconds / 60);
+		seconds = seconds % 60;
 
-    return `${days} DÍAS Y ${hours.toString().padStart(2, "0")}:${minutes
-      .toString()
-      .padStart(2, "0")}:${seconds.toFixed(0).toString().padStart(2, "0")}`;
-  };
+		return `${days} DÍAS Y ${hours.toString().padStart(2, "0")}:${minutes
+			.toString()
+			.padStart(2, "0")}:${seconds.toFixed(0).toString().padStart(2, "0")}`;
+	};
 
-  return <div>{formatTime(time)}</div>;
+	return <div>{formatTime(time)}</div>;
 };
 
 // ------------component-----------------
 
 const ToysCampaign = () => {
-  return (
-    <div className="toys__container">
-      <div className="toys__title">
-        <div>Colecta de juguetes</div>
-        <div className="toys__title-images">
-          <img src={Toy1} alt="toy icon 1" />
-          <img src={Toy2} alt="toy icon 2" />
-          <img src={Toy3} alt="toy icon 3" />
-          <img src={Toy4} alt="toy icon 4" />
-          <img src={Toy5} alt="toy icon 5" />
-          <img src={Toy6} alt="toy icon 6" />
-        </div>
-      </div>
-      <WavesBlue className="toys__waves-blue" />
-      <div className="toys__content">
-        <div>
-          <div>Trae tu donativo</div>
-          <div className="toys__date">30 de Diciembre</div>
-          <div className="toys__hour">17:30 horas</div>
-          <div className="toys__place">Sede SomosMas</div>
-          <div className="toys__counter">
-            <CountDownTimer />
-          </div>
-        </div>
+	return (
+		<div className="toys__container">
+			<div className="toys__title">
+				<div>Colecta de juguetes</div>
+				<div className="toys__title-images">
+					<img src={Toy1} alt="toy icon 1" />
+					<img src={Toy2} alt="toy icon 2" />
+					<img src={Toy3} alt="toy icon 3" />
+					<img src={Toy4} alt="toy icon 4" />
+					<img src={Toy5} alt="toy icon 5" />
+					<img src={Toy6} alt="toy icon 6" />
+				</div>
+			</div>
+			<WavesBlue className="toys__waves-blue" />
+			<div className="toys__content">
+				<div>
+					<div>Trae tu donativo</div>
+					<div className="toys__date">30 de Diciembre</div>
+					<div className="toys__hour">17:30 horas</div>
+					<div className="toys__place">Sede SomosMas</div>
+					<div className="toys__counter">
+						<CountDownTimer />
+					</div>
+				</div>
 
-        <div className="toys__images-container">
-          <img src={KidsImage} alt="kids" />
-        </div>
-      </div>
+				<div className="toys__images-container">
+					<img src={KidsImage} alt="kids" />
+				</div>
+			</div>
 
-      <img src={ToysBackground} alt="background" />
-      <WavesRed className="toys__waves-red" />
-    </div>
-  );
+			<img src={ToysBackground} alt="background" />
+			<WavesRed className="toys__waves-red" />
+		</div>
+	);
 };
 
 export default ToysCampaign;
->>>>>>> 6f0a98d32a5cf08ce4e2744e5f154ede95c1c3b0
