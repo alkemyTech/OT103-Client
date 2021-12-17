1532434e034747b47b775d3962dca84a39fe6547
Fausto Rossi <faustorossi@usal.edu.ar>
2021-12-17 20:13:37 -0300

    slider & mobile first

--- src/Campaigns/Toys/ToysCampaign.js
+++ src/Campaigns/Toys/ToysCampaign.js
@@ -12,37 +12,38 @@ import { ReactComponent as WavesBlue } from "../../assets/waves-blue.svg";
 import "../../Components/Footer/FooterPublic.scss";
 import { useEffect, useState } from "react";
 import FooterPublic from "../../Components/Footer/FooterPublic";
+import Slider from "./Slider";
 
 // -------------timer---------------
 
 const CountDownTimer = () => {
-  // timer to 31 december at 17:30
-  const remainingTime = (+new Date("2021-12-31T17:30:59") - +new Date()) / 1000;
+	// timer to 31 december at 17:30
+	const remainingTime = (+new Date("2021-12-31T17:30:59") - +new Date()) / 1000;
 
-  const [time, setTime] = useState(remainingTime);
+	const [time, setTime] = useState(remainingTime);
 
-  useEffect(() => {
-    const timerId = setInterval(() => {
-      setTime((prevState) => prevState - 1);
-    }, 1000);
-    return () => clearInterval(timerId);
-  });
+	useEffect(() => {
+		const timerId = setInterval(() => {
+			setTime((prevState) => prevState - 1);
+		}, 1000);
+		return () => clearInterval(timerId);
+	});
 
-  // format seconds to DD HH:MM:SS
-  const formatTime = (seconds) => {
-    const days = Math.floor(seconds / 86400);
-    seconds = seconds % 86400;
-    const hours = Math.floor(seconds / 3600);
-    seconds = seconds % 3600;
-    const minutes = Math.floor(seconds / 60);
-    seconds = seconds % 60;
+	// format seconds to DD HH:MM:SS
+	const formatTime = (seconds) => {
+		const days = Math.floor(seconds / 86400);
+		seconds = seconds % 86400;
+		const hours = Math.floor(seconds / 3600);
+		seconds = seconds % 3600;
+		const minutes = Math.floor(seconds / 60);
+		seconds = seconds % 60;
 
-    return `${days} DÍAS Y ${hours.toString().padStart(2, "0")}:${minutes
-      .toString()
-      .padStart(2, "0")}:${seconds.toFixed(0).toString().padStart(2, "0")}`;
-  };
+		return `${days} DÍAS Y ${hours.toString().padStart(2, "0")}:${minutes
+			.toString()
+			.padStart(2, "0")}:${seconds.toFixed(0).toString().padStart(2, "0")}`;
+	};
 
-  return <div>{formatTime(time)}</div>;
+	return <div>{formatTime(time)}</div>;
 };
 
 // ------------component-----------------
@@ -78,6 +79,7 @@ const ToysCampaign = () => {
 						<img src={KidsImage} alt="kids" />
 					</div>
 				</div>
+				<Slider />
 
 				<img src={ToysBackground} alt="background" />
 				<WavesRed className="toys__waves-red" />
