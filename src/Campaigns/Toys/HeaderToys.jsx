a58b28b4128944589ea57ff63937eeda69a4176c
Guido Fortunato <guido_fortunato@hotmail.com.ar>
2021-12-18 03:11:43 -0300

    agrego estilos

--- src/Campaigns/Toys/HeaderToys.jsx
+++ src/Campaigns/Toys/HeaderToys.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import logo from '../../assets/images/logo.png'
+import logojuguetes from '../../assets/images/toys/logojuguetes.png'
 import './headerToys.scss'
 
 const HeaderToys = () => {
   return (
     <header className='header__toys'>
       <div className="header__toys-content">
-        <img src={logo} alt="logo" className='header__toys-logo' />
+        <img src={logojuguetes} alt="logo" className='header__toys-logo' />
       </div>
       <div className="header__toys-ong">
         <img src={ logo } alt="logo ONG"  className='header__toys-logo-ong'/>
       </div>
       <div className="header__toys-slogan">
-        <h5 className="slogan">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae voluptatibus cupiditate vel maiores accusantium autem expedita, saepe alias molestias in sit provident.</h5>
+        <h5 className="slogan">Los chicos te necesitan hoy</h5>
+        <h5 className="slogan">Sumate con tu donación !!</h5>
       </div>
 
     </header>
