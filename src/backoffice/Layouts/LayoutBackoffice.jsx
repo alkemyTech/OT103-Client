65bbb373edf9835b6a7e75150b008c06e942d61f
Julian Kominovic <juliankominovic@gmail.com>
2021-12-19 21:39:45 -0300

    Layout created

---
+++ src/backoffice/Layouts/LayoutBackoffice.jsx
@@ -0,0 +1,13 @@
+/*eslint-disable */
+import React from "react";
+import Header from "../../Components/Header/Header";
+const LayoutBackoffice = ({ children }) => {
+	return (
+		<>
+			<Header />
+			{children}
+		</>
+	);
+};
+
+export default LayoutBackoffice;
