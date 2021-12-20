32a469a155b06c1eae88645017b9ff425f344dbf
Guido Fortunato <guido_fortunato@hotmail.com.ar>
2021-12-20 15:13:40 -0300

    resuelvo conflictos

--- src/Components/context/LoginProvider.js
+++ src/Components/context/LoginProvider.js
@@ -1,34 +1,16 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext,  useState } from "react";
 
-export const LoginContext = createContext()
+export const LoginContext = createContext();
 
 const LoginProvider = (props) => {
 
 
-    const [login, setLogin] = useState(false)
+	const [login, setLogin] = useState(false);
+	return (
+		<LoginContext.Provider value={{login, setLogin}}>
+			{props.children}
+		</LoginContext.Provider>
+	);
+};
 
-    // useEffect(() => {
-        
-    //     if (localStorage.getItem('tempToken')) {
-    //         const loginValue = JSON.parse(localStorage.getItem('tempToken'))
-    //         setLogin(loginValue)
-    //     } 
-
-
-    // }, [])
-
-    // const cambiarLogin = valor => {
-    //     setLogin(valor)
-    //     localStorage.setItem('tempToken', JSON.stringify(valor))
-    // }
-   
-
-
-    return (
-        <LoginContext.Provider value={{login, setLogin}}>
-            {props.children}
-        </LoginContext.Provider>
-    )
-}
-
-export default LoginProvider
+export default LoginProvider;
