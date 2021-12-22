7c5d53e57c56d0a674c26b43e2b21151d4da3f88
Julian Kominovic <juliankominovic@gmail.com>
2021-12-22 20:37:42 -0300

    Public routes fixed

--- src/Routes/Public.jsx
+++ src/Routes/Public.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense } from "react";
 import { Route } from "react-router-dom";
 import { AnimatedSwitch } from "react-router-transition";
+import DonationsGreet from "../Components/Donations/DonationsGreet";
 import LayoutPublic from "../Components/Layout/LayoutPublic";
 import LoadingSpinner from "../Components/Spinner/LoadingSpinner";
 import { mapStyles } from "../helpers/routerTransitions";
@@ -12,7 +13,6 @@ import {
 	CategoriesForm,
 	Contact,
 	ContactForm,
-	DonationsGreet,
 	Home,
 	LoginForm,
 	Members,
@@ -49,8 +49,6 @@ function Public() {
 					/>
 					<Route exact path="/create-user" component={UserForm} />
 					<Route exact path="/edit-user/:id" component={UserForm} />
-					<Route exact path="/school-campaign" component={SchoolCampaign} />
-					<Route exact path="/toys-campaign" component={ToysCampaign} />
 					<Route exact path="/create-member" component={MembersForm} />
 					<Route exact path="/create-project" component={ProjectsForm} />
 					<Route exact path="/about" component={AboutMain} />
