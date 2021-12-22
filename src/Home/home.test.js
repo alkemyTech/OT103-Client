ffba6885e42fffc3cf0acc495681b2d1ea471bff
FlaviaCohen <flaviacohen1988@gmail.com>
2021-12-22 13:49:19 -0300

    home test deleted

--- src/Home/home.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Home from './Home'
-
-test('renders Bienvenidos', () => {
-    render(<Home/>);
-    const Saludo = screen.getAllByText('Bienvenidos!');
-    expect(Saludo).toBeInTheDocument();
-})
