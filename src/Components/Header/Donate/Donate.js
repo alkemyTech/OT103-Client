284b162cc36ec15dafc96216a232cf0ff80b3fb7
Fausto Rossi <faustorossi@usal.edu.ar>
2021-12-23 17:07:47 -0300

    button style

--- src/Components/Header/Donate/Donate.js
+++ src/Components/Header/Donate/Donate.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Donate.scss";
+import { GiReceiveMoney } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 function Donate() {
 	return (
 		<div>
-			<button>Donar</button>
+			<Link to="/donate">
+				<button className="donate__button">
+					Donar
+					<i className="fas fa-donate"></i>
+				</button>
+			</Link>
 		</div>
 	);
 }
