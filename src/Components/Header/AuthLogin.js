542b4d8a4e1d8e38a1f3ba3e5852025fa32fd05a
David Caro <david.caro.dec@hotmail.com>
2021-12-23 16:49:11 -0300

    login and register buttons

--- src/Components/Header/AuthLogin.js
+++ src/Components/Header/AuthLogin.js
@@ -3,10 +3,17 @@ import { NavLink } from 'react-router-dom'
 
 export const AuthLogin = () => {
     return (
-        <li className="nav-item mb-2 mb-lg-0 mb-lg-0">
-            <NavLink className="nav-link me-3 mx-lg-5" to="/" exact >
-                Login
-            </NavLink>
-        </li>
+        <>
+            <li className="nav-item mb-2 mb-lg-0 mb-lg-0">
+                <NavLink className="nav-link me-3 mx-lg-5" to="/login-form" exact >
+                    Login
+                </NavLink>
+            </li>
+            <li className="nav-item mb-2 mb-lg-0 mb-lg-0">
+                <NavLink className="nav-link me-3 me-lg-5" to="/register-form" exact >
+                    Registrate
+                </NavLink>
+            </li>
+        </>
     )
 }
