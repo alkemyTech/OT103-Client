c4653d78acbb5cf0c37bc2a95143a14c69cc0f2d
Luis Ramiro Krupoviesa <krupoviesaramiro@gmail.com>
2021-12-27 20:13:21 -0300

    add backoffice header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,67 +5,76 @@ import logo from "../../assets/images/logo.png";
 import { AuthLogout } from "./AuthLogout";
 
 const Header = () => {
-	const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
 
-	const showMenu = () => {
-		setMenuIsOpen((prev) => !prev);
-	};
+  const showMenu = () => {
+    setMenuIsOpen((prev) => !prev);
+  };
 
-	const data = [
-		{
-			text: "Actividades",
-			link: "/backoffice/activities",
-		},
-		{
-			text: "Novedades",
-			link: "/backoffice/news",
-		},
-		{
-			text: "Miembros",
-			link: "/backoffice/members",
-		},
-		{
-			text: "Organización",
-			link: "/backoffice/organization",
-		},
-		{
-			text: "Slides",
-			link: "/backoffice/slices",
-		},
-		{
-			text: "Usuarios",
-			link: "/backoffice/users",
-		},
-	];
+  const data = [
+    {
+      text: "Novedades",
+      link: "/backoffice/news",
+    },
+    {
+      text: "Actividades",
+      link: "/backoffice/activities",
+    },
+    {
+      text: "Categorías",
+      link: "/backoffice/categories",
+    },
+    {
+      text: "Testimonios",
+      link: "/backoffice/testimonials",
+    },
+    {
+      text: "Organización",
+      link: "/backoffice/organization",
+    },
+    {
+      text: "Slides",
+      link: "/backoffice/slides",
+    },
+    {
+      text: "Usuarios",
+      link: "/backoffice/users",
+    },
+    {
+      text: "Miembros",
+      link: "/backoffice/members",
+    },
+  ];
 
-	return (
-		<nav className="header__container">
-			<Link to="/">
-				<img src={logo} alt="logo" width={130} className="header__logo" />
-			</Link>
+  return (
+    <nav className="headerBackoffice header__container">
+      <Link to="/backoffice">
+        <img src={logo} alt="logo" width={130} className="header__logo" />
+      </Link>
 
-			<button
-				className="header__menuBtn form__btn-secondary"
-				onClick={showMenu}
-			>
-				MENU
-			</button>
-			<div className="header__menuPanel">
-				<ul className="header__navbar">
-					{data.map((item, index) => (
-						<li key={index}>
-							<NavLink to={item.link} exact className="header__link">
-								{item.text}
-							</NavLink>
-						</li>
-					))}
-				</ul>
+      <button
+        className="header__menuBtn form__btn-secondary"
+        onClick={showMenu}
+      >
+        MENU
+      </button>
+      <div className={`header__menuPanel ${menuIsOpen && "active"}`}>
+        <ul className="header__navbar">
+          {data.map((item, index) => (
+            <li key={index}>
+              <NavLink to={item.link} exact className="header__link">
+                {item.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
 
-				<div className="header__btnContainer">
-					<AuthLogout />
-				</div>
-			</div>
-		</nav>
-	);
+        <div className="header__btnContainer">
+          <AuthLogout />
+        </div>
+      </div>
+      <i className="fas fa-pen pencilIcon"></i>
+    </nav>
+  );
 };
 export default Header;
