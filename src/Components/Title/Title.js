5202cb8ef4420b8307c0482017952f0464109651
Julian Kominovic <juliankominovic@gmail.com>
2021-12-30 21:06:13 -0300

    404 image changed

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -3,7 +3,7 @@ import defaultImage from "../../assets/images/404.png";
 export const Title = ({ title, image }) => {
 	return (
 		<div>
-			<img src={!image ? defaultImage : image} alt="" className="image-300px" />
+			<img src={!image ? defaultImage : image} alt="" />
 			<h1 className="main-title">{title}</h1>
 		</div>
 	);
